7d34ef8411a1776592c9142c2459d05d6670ab66
Jorge Linares <jorge.im.linares@gmail.com>
2024-10-27 00:48:13 -0300

    First test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,53 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Card from "$lib/Card.svelte";
+    import ExpandedCardTarget from "$lib/ExpandedCardTarget.svelte";
+	import { onMount } from "svelte";
+
+    let cards:Array<any> = Array(23)
+
+    
+</script>
+
+<div>
+    <ExpandedCardTarget>
+        {#each cards as card, i}
+            <Card jsStyle={{easing:"expoOut"}}>
+                <div slot="details" class="card-details">
+                    Here goes all the necessary details for the card
+                    <h1>{i}</h1>
+                </div>
+            </Card>
+        {/each}
+    </ExpandedCardTarget>
+</div>
+
+<style>
+    :global(body) {
+        margin: 0;
+    }
+
+    :global(.expanded-target) {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        align-content: stretch;
+        gap: 1vh;
+        padding: 1vh;
+        box-sizing: content-box;
+        margin: 1vh;
+    }
+    :global(.expanding-card) {
+        flex: 1;
+    }
+
+    div {
+        height: 100%;
+    }
+
+    .card-details {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
